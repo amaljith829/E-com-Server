0839445d8bbea78b7db15538610d37e82181f7ac
Amaljith A A <amaljith829@gmail.com>
2025-04-23 12:00:10 +0530

    cart functionality

--- controller/cartController.js
+++ controller/cartController.js
@@ -4,9 +4,8 @@ const Product = require('../models/productModel');
 
 const addToCart = async (req, res) => {
     try {
-        const { product, price, quantity } = req.body;
-        const newCart = new Cart({ product, price, quantity });
-        await newCart.save();
+        console.log(req.user);
+        // add to cart functionality
         res.status(201).json({ message: 'Product added to cart successfully', cart: newCart });
         console.log(req.body)
     } catch (error) {
